e82de1880af3bcd8955bc0af7378269479cfe20b
hovo0 <hovo.asatryan2001@gmail.com>
2022-02-05 14:20:34 +0400

    Added 'correct/incorrect' button settings

--- QuizWeb/game.js
+++ QuizWeb/game.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question")
 const choices = document.getElementsByClassName("choice-text")
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -498,18 +498,45 @@ startGame = () => {
     getNewQuestion();
 };
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign("/end.html");
+    }
+
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.foreach(choice => {
+    Array.from(choices).forEach(choice => {
 
         const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number]
+        choice.innerText = currentQuestion["choice" + number];
 
     });
-};
 
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+Array.from(choices).forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers)
+            return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        selectedChoice.parentElement.classList.add(classToApply);
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            /*if (selectedChoice === 'incorrect') {
+                return window.location.assign("/end.html");
+            }*/
+            getNewQuestion();
+        }, 1000);
+
+    });
+});
 
 startGame();
